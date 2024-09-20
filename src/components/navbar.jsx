ed8f6f4ea6fb04b2fcfffc62731fa91fa16a6137
Tushar Thanvi <143414158+TusharThanvi1990@users.noreply.github.com>
2024-09-20 15:38:03 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <header className='w-full py-5 px-5 sm:px10 flex justify-between items-center '>
       <nav className='flex w-full screen-max-width'>
-          <img src={appleImg} alt='Apple' width={14} height={18}/>
+{/*           <img src={appleImg} alt='Apple' width={14} height={18}/> */}
           <div className='flex flex-1 justify-center max-sm:hidden'>
           {navLists.map((nav) => (
               <div key = {nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
